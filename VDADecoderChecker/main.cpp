05c3ec9002ee1457d1e25233bf4abb78e67a010b
cylonbrain <cylonbrain@gmail.com>
2011-06-23 16:26:48 +0200

    Initial Commit

---
+++ VDADecoderChecker/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VDADecoderChecker
+//
+//  Created by Andy Breuhan on 23.06.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
